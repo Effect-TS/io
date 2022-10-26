b4daaaea31e75151ce30e6d164039880e1abaff1
Michael Arnaldi <arnaldimichael@gmail.com>
2022-10-26 22:58:20 +0000

    add unused import removal

--- examples/traced.ts
+++ examples/traced.ts
@@ -5,6 +5,6 @@ runtimeDebug.traceEnabled = true
 
 // Rest
 import * as E from "@effect/io/Effect"
-import { pipe } from "@fp-ts/data/Function"
+import * as F from "@fp-ts/data/Function"
 
-console.log(pipe(E.succeed(0), E.flatMap((n) => E.succeed(n + 1))))
+console.log(F.pipe(E.succeed(0), E.flatMap((n) => E.succeed(n + 1))))
