9a4929783efa79af7ffa24d203cc458d49782074
Michael Arnaldi <arnaldimichael@gmail.com>
2022-10-26 23:33:55 +0000

    add debug model and comments

--- src/Debug.ts
+++ src/Debug.ts
@@ -4,25 +4,73 @@
 import type { Level } from "@effect/io/Logger/Level"
 
 /**
+ * @since 1.0.0
+ * @category models
+ */
+export interface Debug {
+  /**
+   * Overrides the default log level filter for loggers such as console.
+   */
+  logLevelOverride: Level | undefined
+  /**
+   * When specified it will be used to collect call traces at runtime.
+   *
+   * NOTE: Collecting traces at runtime is expensive and unreliable due
+   * to the stack trace format being non standardized across platforms.
+   * This flag is meant to be used only when debugging during development.
+   */
+  traceExtractor: ((at: number) => string | undefined) | undefined
+  /**
+   * A function that is used to filter which traces to show and collect.
+   */
+  traceFilter: (trace: string) => boolean
+  /**
+   * Enables execution tracing in the fiber runtime.
+   */
+  traceExecutionEnabled: boolean
+  /**
+   * Renders the execution trace in the error cause when it is available.
+   */
+  traceExecutionEnabledInCause: boolean
+  /**
+   * Renders the stack trace in the error cause when it is available.
+   */
+  traceStackEnabledInCause: boolean
+  /**
+   * Renders the span trace in the error cause when it is available.
+   */
+  traceSpanEnabledInCause: boolean
+  /**
+   * Sets a limit on how many execution traces should be rendered.
+   */
+  traceExecutionLimit: number
+  /**
+   * Sets a limit on how many stack traces should be rendered.
+   */
+  traceStackLimit: number
+  /**
+   * Sets a limit on how many span traces should be rendered.
+   */
+  traceSpanLimit: number
+  /**
+   * Enables debug logging of execution traces.
+   */
+  traceExecutionLogEnabled: boolean
+  /**
+   * Enables tracing.
+   */
+  traceEnabled: boolean
+  /**
+   * Listens to execution traces.
+   */
+  traceExecutionHook: Array<(trace: string) => void>
+}
+
+/**
  * @category debug
  * @since 1.0.0
  */
-export const runtimeDebug: {
-  logLevelOverride: Level | undefined
-  traceExtractor: ((at: number) => string | undefined) | undefined
-  traceFilter: (trace: string) => boolean
-  traceExecutionEnabled: boolean
-  traceExecutionEnabledInCause: boolean
-  traceStackEnabledInCause: boolean
-  traceSpanEnabledInCause: boolean
-  traceExecutionLimit: number
-  traceStackLimit: number
-  traceSpanLimit: number
-  traceExecutionLogEnabled: boolean
-  traceEnabled: boolean
-  traceExecutionHook: Array<(trace: string) => void>
-  defaultSpanTracer: "identity" | "cause"
-} = {
+export const runtimeDebug: Debug = {
   logLevelOverride: undefined,
   traceExecutionEnabled: false,
   traceExecutionLogEnabled: false,
@@ -35,8 +83,7 @@ export const runtimeDebug: {
   traceExtractor: undefined,
   traceFilter: () => true,
   traceEnabled: false,
-  traceExecutionHook: [],
-  defaultSpanTracer: "identity"
+  traceExecutionHook: []
 }
 
 /**
