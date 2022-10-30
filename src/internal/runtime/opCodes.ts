04211b1260d6c20ddee835920e72c901e9ec2d52
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-10-30 01:43:47 +0000

    isolate op codes for runtime

---
+++ src/internal/runtime/opCodes.ts
@@ -0,0 +1,65 @@
+/** @internal */
+export type OP_ASYNC = typeof OP_ASYNC
+
+/** @internal */
+export const OP_ASYNC = 0 as const
+
+/** @internal */
+export type OP_FAILURE = typeof OP_FAILURE
+
+/** @internal */
+export const OP_FAILURE = 1 as const
+
+/** @internal */
+export type OP_ON_FAILURE = typeof OP_ON_FAILURE
+
+/** @internal */
+export const OP_ON_FAILURE = 2 as const
+
+/** @internal */
+export type OP_ON_SUCCESS = typeof OP_ON_SUCCESS
+
+/** @internal */
+export const OP_ON_SUCCESS = 3 as const
+
+/** @internal */
+export type OP_ON_SUCCESS_AND_FAILURE = typeof OP_ON_SUCCESS_AND_FAILURE
+
+/** @internal */
+export const OP_ON_SUCCESS_AND_FAILURE = 4 as const
+
+/** @internal */
+export type OP_SUCCESS = typeof OP_SUCCESS
+
+/** @internal */
+export const OP_SUCCESS = 5 as const
+
+/** @internal */
+export type OP_SYNC = typeof OP_SYNC
+
+/** @internal */
+export const OP_SYNC = 6 as const
+
+/** @internal */
+export type OP_UPDATE_RUNTIME_FLAGS = typeof OP_UPDATE_RUNTIME_FLAGS
+
+/** @internal */
+export const OP_UPDATE_RUNTIME_FLAGS = 7 as const
+
+/** @internal */
+export type OP_WHILE = typeof OP_WHILE
+
+/** @internal */
+export const OP_WHILE = 8 as const
+
+/** @internal */
+export type OP_WITH_RUNTIME = typeof OP_WITH_RUNTIME
+
+/** @internal */
+export const OP_WITH_RUNTIME = 9 as const
+
+/** @internal */
+export type OP_YIELD = typeof OP_YIELD
+
+/** @internal */
+export const OP_YIELD = 10 as const
