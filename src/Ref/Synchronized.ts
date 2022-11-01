b9df5cb57f29cbb8ae855eb77aad0a490c06d8a6
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-11-01 16:12:41 +0000

    add Synchronized ref

--- src/Ref/Synchronized.ts
+++ src/Ref/Synchronized.ts
@@ -2,36 +2,40 @@
  * @since 1.0.0
  */
 import type * as Effect from "@effect/io/Effect"
-import * as internal from "@effect/io/internal/ref"
+import * as internal from "@effect/io/internal/synchronizedRef"
+import type * as Ref from "@effect/io/Ref"
 
 /**
  * @since 1.0.0
  * @category symbols
  */
-export const RefTypeId: unique symbol = internal.RefTypeId
+export const SynchronizedTypeId: unique symbol = internal.SynchronizedTypeId
 
 /**
  * @since 1.0.0
  * @category symbols
  */
-export type RefTypeId = typeof RefTypeId
+export type SynchronizedTypeId = typeof SynchronizedTypeId
 
 /**
  * @since 1.0.0
  * @category models
  */
-export interface Ref<A> extends Ref.Variance<A> {
+export interface Synchronized<A> extends Synchronized.Variance<A>, Ref.Ref<A> {
   /** @internal */
-  modify<B>(f: (a: A) => readonly [B, A]): Effect.Effect<never, never, B>
+  modifyEffect<R, E, B>(f: (a: A) => Effect.Effect<R, E, readonly [B, A]>): Effect.Effect<R, E, B>
 }
 
 /**
  * @since 1.0.0
- * @category models
  */
-export namespace Ref {
+export declare namespace Synchronized {
+  /**
+   * @since 1.0.0
+   * @category models
+   */
   export interface Variance<A> {
-    readonly [RefTypeId]: {
+    readonly [SynchronizedTypeId]: {
       readonly _A: (_: never) => A
     }
   }
@@ -70,7 +74,21 @@ export const getAndUpdate = internal.getAndUpdate
  * @since 1.0.0
  * @category mutations
  */
+export const getAndUpdateEffect = internal.getAndUpdateEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
 export const getAndUpdateSome = internal.getAndUpdateSome
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
+export const getAndUpdateSomeEffect = internal.getAndUpdateSomeEffect
 
 /**
  * @macro traced
@@ -84,7 +102,21 @@ export const modify = internal.modify
  * @since 1.0.0
  * @category mutations
  */
+export const modifyEffect = internal.modifyEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
 export const modifySome = internal.modifySome
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
+export const modifySomeEffect = internal.modifySomeEffect
 
 /**
  * @macro traced
@@ -112,6 +144,20 @@ export const update = internal.update
  * @since 1.0.0
  * @category mutations
  */
+export const updateEffect = internal.updateEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
+export const updateAndGetEffect = internal.updateAndGetEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
 export const updateSome = internal.updateSome
 
 /**
@@ -119,7 +165,21 @@ export const updateSome = internal.updateSome
  * @since 1.0.0
  * @category mutations
  */
+export const updateSomeEffect = internal.updateSomeEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
 export const updateSomeAndGet = internal.updateSomeAndGet
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category mutations
+ */
+export const updateSomeAndGetEffect = internal.updateSomeAndGetEffect
 
 /**
  * @since 1.0.0
