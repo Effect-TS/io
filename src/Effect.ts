cc592237efe5d7c3e55dbf89a7cd3ce73b8e7e31
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-11-01 13:21:50 +0000

    add more Effect combinators

--- src/Effect.ts
+++ src/Effect.ts
@@ -1,7 +1,9 @@
 /**
  * @since 1.0.0
  */
+import type { Cause } from "@effect/io/Cause"
 import * as core from "@effect/io/internal/core"
+import * as effect from "@effect/io/internal/effect"
 import type { Equal } from "@fp-ts/data/Equal"
 
 /**
@@ -18,9 +20,21 @@ export type EffectTypeId = typeof EffectTypeId
 
 /**
  * @since 1.0.0
+ * @category symbols
+ */
+export const EffectErrorTypeId: unique symbol = effect.EffectErrorTypeId
+
+/**
+ * @since 1.0.0
+ * @category symbols
+ */
+export type EffectErrorTypeId = typeof EffectErrorTypeId
+
+/**
+ * @since 1.0.0
  * @category models
  */
-export interface Effect<R, E, A> extends Variance<R, E, A>, Equal {
+export interface Effect<R, E, A> extends Effect.Variance<R, E, A>, Equal {
   /** @internal */
   traced(trace: string | undefined): Effect<R, E, A>
 }
@@ -29,28 +43,112 @@ export interface Effect<R, E, A> extends Variance<R, E, A>, Equal {
  * @since 1.0.0
  * @category models
  */
-export interface Variance<R, E, A> {
-  readonly [EffectTypeId]: {
-    readonly _R: (_: never) => R
-    readonly _E: (_: never) => E
-    readonly _A: (_: never) => A
-  }
+export interface EffectError<E> {
+  readonly [EffectErrorTypeId]: EffectErrorTypeId
+  readonly _tag: "EffectError"
+  readonly cause: Cause<E>
 }
 
 /**
+ * @since 1.0.0
+ */
+export declare namespace Effect {
+  /**
+   * @since 1.0.0
+   * @category models
+   */
+  export interface Variance<R, E, A> {
+    readonly [EffectTypeId]: {
+      readonly _R: (_: never) => R
+      readonly _E: (_: never) => E
+      readonly _A: (_: never) => A
+    }
+  }
+
+  /**
+   * @since 1.0.0
+   * @category models
+   */
+  export type Error<E> = EffectError<E>
+}
+
+/**
+ * Returns `true` if the specified value is an `Effect`, `false` otherwise.
+ *
  * @since 1.0.0
  * @category refinements
  */
 export const isEffect = core.isEffect
 
 /**
+ * Returns `true` if the specified value is an `EffectError`, `false` otherwise.
+ *
+ * @since 1.0.0
+ * @category refinements
+ */
+export const isEffectError = effect.isEffectError
+
+/**
+ * Constructs a new `EffectError`.
+ *
+ * @since 1.0.0
+ * @category constructors
+ */
+export const makeEffectError = effect.makeEffectError
+
+/**
+ * Adds a finalizer to the scope of this effect. The finalizer is guaranteed
+ * to be run when the scope is closed and may depend on the `Exit` value that
+ * the scope is closed with.
+ *
  * @macro traced
  * @since 1.0.0
  * @category finalization
  */
-export const addFinalizerExit = core.addFinalizerExit
+export const addFinalizer = core.addFinalizer
 
 /**
+ * Submerges the error case of an `Either` into an `Effect`. The inverse
+ * operation of `either`.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category error handling
+ */
+export const absolve = effect.absolve
+
+/**
+ * Attempts to convert defects into a failure, throwing away all information
+ * about the cause of the failure.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category error handling
+ */
+export const absorb = effect.absorb
+
+/**
+ * Attempts to convert defects into a failure with the specified function,
+ * throwing away all information about the cause of the failure.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category error handling
+ */
+export const absorbWith = effect.absorbWith
+
+/**
+ * Constructs a scoped resource from an `acquire` and `release` effect.
+ *
+ * If `acquire` successfully completes execution then `release` will be added to
+ * the finalizers associated with the scope of this effect and is guaranteed to
+ * be run when the scope is closed.
+ *
+ * The `acquire` and `release` effects will be run uninterruptibly.
+ *
+ * Additionally, the `release` effect may depend on the `Exit` value specified
+ * when the scope is closed.
+ *
  * @macro traced
  * @since 1.0.0
  * @category constructors
@@ -58,6 +156,45 @@ export const addFinalizerExit = core.addFinalizerExit
 export const acquireRelease = core.acquireRelease
 
 /**
+ * A variant of `acquireRelease` that allows the `acquire` effect to be
+ * interruptible.
+ *
+ * Since the `acquire` effect could be interrupted after partially acquiring
+ * resources, the `release` effect is not allowed to* access the resource
+ * produced by `acquire` and must independently determine what finalization,
+ * if any, needs to be performed (e.g. by examining in memory state).
+ *
+ * Additionally, the `release` effect may depend on the `Exit` value specified
+ * when the scope is closed.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category acquire/release
+ */
+export const acquireReleaseInterruptible = effect.acquireReleaseInterruptible
+
+/**
+ * When this effect represents acquisition of a resource (for example, opening
+ * a file, launching a thread, etc.), `acquireUseRelease` can be used to
+ * ensure the acquisition is not interrupted and the resource is always
+ * released.
+ *
+ * The function does two things:
+ *
+ *   1. Ensures this effect, which acquires the resource, will not be
+ *      interrupted. Of course, acquisition may fail for internal reasons (an
+ *      uncaught exception).
+ *   2. Ensures the `release` effect will not be interrupted, and will be
+ *      executed so long as this effect successfully
+ *      acquires the resource.
+ *
+ * In between acquisition and release of the resource, the `use` effect is
+ * executed.
+ *
+ * If the `release` effect fails, then the entire effect will fail even if the
+ * `use` effect succeeds. If this fail-fast behavior is not desired, errors
+ * produced by the `release` effect can be caught and ignored.
+ *
  * @macro traced
  * @since 1.0.0
  * @category constructors
@@ -65,6 +202,18 @@ export const acquireRelease = core.acquireRelease
 export const acquireUseRelease = core.acquireUseRelease
 
 /**
+ * Makes an explicit check to see if the fiber has been interrupted, and if
+ * so, performs self-interruption
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
+export const allowInterrupt = effect.allowInterrupt
+
+/**
+ * Maps the success value of this effect to the specified constant value.
+ *
  * @macro traced
  * @since 1.0.0
  * @category mapping
@@ -72,6 +221,62 @@ export const acquireUseRelease = core.acquireUseRelease
 export const as = core.as
 
 /**
+ * Maps the success value of this effect to a `Left` value.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category mapping
+ */
+export const asLeft = effect.asLeft
+
+/**
+ * Maps the error value of this effect to a `Left` value.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category mapping
+ */
+export const asLeftError = effect.asLeftError
+
+/**
+ * Maps the success value of this effect to a `Right` value.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category mapping
+ */
+export const asRight = effect.asRight
+
+/**
+ * Maps the error value of this effect to a `Right` value.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category mapping
+ */
+export const asRightError = effect.asRightError
+
+/**
+ * Maps the success value of this effect to a `Some` value.
+ *
+ * @tsplus getter effect/core/io/Effect asSome
+ * @category mapping
+ * @since 1.0.0
+ */
+export const asSome = effect.asSome
+
+/**
+ * Maps the error value of this effect to a `Some` value.
+ *
+ * @tsplus getter effect/core/io/Effect asSome
+ * @category mapping
+ * @since 1.0.0
+ */
+export const asSomeError = effect.asSomeError
+
+/**
+ * Maps the success value of this effect to `void`.
+ *
  * @macro traced
  * @since 1.0.0
  * @category mapping
@@ -79,13 +284,66 @@ export const as = core.as
 export const asUnit = core.asUnit
 
 /**
+ * Imports an asynchronous side-effect into a pure `Effect` value. See
+ * `asyncMaybe` for the more expressive variant of this function that can
+ * return a value synchronously.
+ *
+ * The callback function `Effect<R, E, A> => void` must be called at most once.
+ *
+ * The `FiberId` of the fiber that may complete the async callback may be
+ * provided to allow for better diagnostics.
+ *
  * @macro traced
  * @since 1.0.0
  * @category constructors
  */
 export const async = core.async
 
+// TODO(Max): after runtime
+// /**
+//  * Converts an asynchronous, callback-style API into an `Effect`, which will
+//  * be executed asynchronously.
+//  *
+//  * With this variant, the registration function may return a an `Effect`.
+//  *
+//  * @macro traced
+//  * @since 1.0.0
+//  * @category constructors
+//  */
+// export const asyncEffect = effect.asyncEffect
+
 /**
+ * Imports an asynchronous effect into a pure `Effect` value, possibly returning
+ * the value synchronously.
+ *
+ * If the register function returns a value synchronously, then the callback
+ * function `Effect<R, E, A> => void` must not be called. Otherwise the callback
+ * function must be called at most once.
+ *
+ * The `FiberId` of the fiber that may complete the async callback may be
+ * provided to allow for better diagnostics.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
+export const asyncOption = effect.asyncOption
+
+/**
+ * Imports an asynchronous side-effect into an effect. The side-effect has
+ * the option of returning the value synchronously, which is useful in cases
+ * where it cannot be determined if the effect is synchronous or asynchronous
+ * until the side-effect is actually executed. The effect also has the option
+ * of returning a canceler, which will be used by the runtime to cancel the
+ * asynchronous effect if the fiber executing the effect is interrupted.
+ *
+ * If the register function returns a value synchronously, then the callback
+ * function `Effect<R, E, A> => void` must not be called. Otherwise the callback
+ * function must be called at most once.
+ *
+ * The `FiberId` of the fiber that may complete the async callback may be
+ * provided to allow for better diagnostics.
+ *
  * @macro traced
  * @since 1.0.0
  * @category constructors
@@ -98,6 +356,24 @@ export const asyncInterrupt = core.asyncInterrupt
  * @category error handling
  */
 export const catchAllCause = core.catchAllCause
+
+/**
+ * Constructs an effect with information about the current `Fiber`.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
+export const descriptor = effect.descriptor
+
+/**
+ * Constructs an effect based on information about the current `Fiber`.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
+export const descriptorWith = effect.descriptorWith
 
 /**
  * @macro traced
@@ -124,17 +400,34 @@ export const dieSync = core.dieSync
  * Effectually accesses the environment of the effect.
  *
  * @macro traced
- * @category environment
  * @since 1.0.0
+ * @category environment
  */
 export const environmentWithEffect = core.environmentWithEffect
 
 /**
  * @macro traced
- * @category environment
  * @since 1.0.0
+ * @category environment
  */
 export const environment = core.environment
+
+/**
+ * Returns an effect that, if this effect _starts_ execution, then the
+ * specified `finalizer` is guaranteed to be executed, whether this effect
+ * succeeds, fails, or is interrupted.
+ *
+ * For use cases that need access to the effect's result, see `onExit`.
+ *
+ * Finalizers offer very powerful guarantees, but they are low-level, and
+ * should generally not be used for releasing resources. For higher-level
+ * logic built on `ensuring`, see the `acquireRelease` family of methods.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category finalization
+ */
+export const ensuring = effect.ensuring
 
 /**
  * @macro traced
@@ -223,17 +516,40 @@ export const foldCauseEffect = core.foldCauseEffect
 /**
  * @macro traced
  * @since 1.0.0
+ * @category error handling
+ */
+export const foldEffect = effect.foldEffect
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category conversions
+ */
+export const fromEither = effect.fromEither
+
+/**
+ * @macro traced
+ * @since 1.0.0
  * @category mapping
  */
 export const map = core.map
+
+/**
+ * Returns an effect with its error channel mapped using the specified function.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category mapping
+ */
+export const mapError = effect.mapError
 
 /**
  * Provides the effect with its required environment, which eliminates its
  * dependency on `R`.
  *
  * @macro traced
- * @category environment
  * @since 1.0.0
+ * @category environment
  */
 export const provideEnvironment = core.provideEnvironment
 
@@ -242,10 +558,19 @@ export const provideEnvironment = core.provideEnvironment
  * leaving the remainder `R0`.
  *
  * @macro traced
- * @category environment
  * @since 1.0.0
+ * @category environment
  */
 export const provideSomeEnvironment = core.provideSomeEnvironment
+
+/**
+ * Exposes the full `Cause` of failure for the specified effect.
+ *
+ * @macro traced
+ * @since 1.0.0
+ * @category error handling
+ */
+export const sandbox = effect.sandbox
 
 /**
  * @macro traced
@@ -253,20 +578,6 @@ export const provideSomeEnvironment = core.provideSomeEnvironment
  * @category environment
  */
 export const scope = core.scope
-
-/**
- * @macro traced
- * @since 1.0.0
- * @category constructors
- */
-export const succeed = core.succeed
-
-/**
- * @macro traced
- * @since 1.0.0
- * @category constructors
- */
-export const sync = core.sync
 
 /**
  * @macro traced
@@ -287,7 +598,21 @@ export const serviceWithEffect = core.serviceWithEffect
  * @since 1.0.0
  * @category constructors
  */
+export const succeed = core.succeed
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
 export const suspendSucceed = core.suspendSucceed
+
+/**
+ * @macro traced
+ * @since 1.0.0
+ * @category constructors
+ */
+export const sync = core.sync
 
 /**
  * @macro traced
---
+++ src/internal/effect.ts
@@ -0,0 +1,260 @@
+import * as Cause from "@effect/io/Cause"
+import type * as Effect from "@effect/io/Effect"
+import type * as Exit from "@effect/io/Exit"
+import type * as Fiber from "@effect/io/Fiber"
+import * as FiberId from "@effect/io/Fiber/Id"
+import * as core from "@effect/io/internal/core"
+import type * as Scope from "@effect/io/Scope"
+import * as Either from "@fp-ts/data/Either"
+import { identity, pipe } from "@fp-ts/data/Function"
+import * as HashSet from "@fp-ts/data/HashSet"
+import * as Option from "@fp-ts/data/Option"
+
+/** @internal */
+const EffectErrorSymbolKey = "@effect/io/Effect/Error"
+
+/** @internal */
+export const EffectErrorTypeId: Effect.EffectErrorTypeId = Symbol.for(
+  EffectErrorSymbolKey
+) as Effect.EffectErrorTypeId
+
+/** @internal */
+export const isEffectError = (u: unknown): u is Effect.EffectError<unknown> => {
+  return typeof u === "object" && u != null && EffectErrorTypeId in u
+}
+
+/** @internal */
+export const makeEffectError = <E>(cause: Cause.Cause<E>): Effect.EffectError<E> => ({
+  [EffectErrorTypeId]: EffectErrorTypeId,
+  _tag: "EffectError",
+  cause
+})
+
+/** @internal */
+export const absolve = <R, E, A>(self: Effect.Effect<R, E, Either.Either<E, A>>): Effect.Effect<R, E, A> => {
+  return pipe(self, core.flatMap(fromEither))
+}
+
+/** @internal */
+export const absorb = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, unknown, A> => {
+  return pipe(self, absorbWith(identity))
+}
+
+/** @internal */
+export const absorbWith = <E>(f: (e: E) => unknown) => {
+  return <R, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, unknown, A> => {
+    return pipe(
+      sandbox(self),
+      foldEffect((cause) => core.fail(pipe(cause, Cause.squashWith(f))), core.succeed)
+    )
+  }
+}
+
+/** @internal */
+export const acquireReleaseInterruptible = <R, E, A, R2, X>(
+  acquire: Effect.Effect<R, E, A>,
+  release: (exit: Exit.Exit<unknown, unknown>) => Effect.Effect<R2, never, X>
+): Effect.Effect<R | R2 | Scope.Scope, E, A> => {
+  return pipe(acquire, ensuring(core.addFinalizer(release)))
+}
+
+/** @internal */
+export const allowInterrupt = (): Effect.Effect<never, never, void> => {
+  return descriptorWith((descriptor) => HashSet.size(descriptor.interruptors) > 0 ? core.interrupt() : core.unit())
+}
+
+/** @internal */
+export const asLeft = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, E, Either.Either<A, never>> => {
+  return pipe(self, core.map(Either.left))
+}
+
+/** @internal */
+export const asLeftError = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, Either.Either<E, never>, A> => {
+  return pipe(self, mapError(Either.left))
+}
+
+/** @internal */
+export const asRight = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, E, Either.Either<never, A>> => {
+  return pipe(self, core.map(Either.right))
+}
+
+/** @internal */
+export const asRightError = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, Either.Either<never, E>, A> => {
+  return pipe(self, mapError(Either.right))
+}
+
+/** @internal */
+export const asSome = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, E, Option.Option<A>> => {
+  return pipe(self, core.map(Option.some))
+}
+
+/** @internal */
+export const asSomeError = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, Option.Option<E>, A> => {
+  return pipe(self, mapError(Option.some))
+}
+
+// TODO(Max): after runtime
+/** @internal */
+// export const asyncEffect = <R, E, A, R2, E2, X>(
+//   register: (callback: (_: Effect.Effect<R, E, A>) => void) => Effect.Effect<R2, E2, X>
+// ): Effect.Effect<R | R2, E | E2, A> => {
+//   return pipe(
+//     core.makeDeferred<E | E2, A>(),
+//     core.flatMap((deferred) =>
+//       pipe(
+//         runtime<R | R2>(),
+//         core.flatMap((runtime) =>
+//           core.uninterruptibleMask((restore) =>
+//             pipe(
+//               restore(
+//                 pipe(
+//                   register((cb) => runtime.unsafeRunAsync(pipe(cb, core.intoDeferred(deferred)))),
+//                   core.catchAllCause((cause) => pipe(deferred, core.failCauseDeferred(cause as Cause.Cause<E | E2>)))
+//                 )
+//               ),
+//               fork,
+//               core.zipRight(core.awaitDeferred(deferred))
+//             )
+//           )
+//         )
+//       )
+//     )
+//   )
+// }
+
+/** @internal */
+export const asyncOption = <R, E, A>(
+  register: (callback: (_: Effect.Effect<R, E, A>) => void) => Option.Option<Effect.Effect<R, E, A>>,
+  blockingOn: FiberId.FiberId = FiberId.none
+): Effect.Effect<R, E, A> => {
+  return core.asyncInterrupt(
+    (cb) => {
+      const option = register(cb)
+      switch (option._tag) {
+        case "None": {
+          return Either.left(core.unit())
+        }
+        case "Some": {
+          return Either.right(option.value)
+        }
+      }
+    },
+    blockingOn
+  )
+}
+
+/**
+ * Imports a synchronous side-effect into a pure `Effect` value, translating any
+ * thrown exceptions into typed failed effects creating with `Effect.fail`.
+ *
+ * @tsplus static effect/core/io/Effect.Ops attempt
+ * @category constructors
+ * @since 1.0.0
+ */
+export const attempt = <A>(evaluate: () => A): Effect.Effect<never, unknown, A> => {
+  return core.sync(() => {
+    try {
+      return evaluate()
+    } catch (error) {
+      throw makeEffectError(Cause.fail(error))
+    }
+  })
+}
+
+/** @internal */
+export const descriptor = (): Effect.Effect<never, never, Fiber.Fiber.Descriptor> => {
+  return descriptorWith(core.succeed)
+}
+
+/** @internal */
+export const descriptorWith = <R, E, A>(
+  f: (descriptor: Fiber.Fiber.Descriptor) => Effect.Effect<R, E, A>
+): Effect.Effect<R, E, A> => {
+  // TODO(Max): remove cast to any
+  return core.withFiberRuntime((state, status) => {
+    return f({
+      id: (state as any).id as any,
+      status,
+      interruptors: Cause.interruptors((state as any).getFiberRef(core.interruptedCause))
+    })
+  })
+}
+
+/** @internal */
+export const ensuring = <R1, X>(finalizer: Effect.Effect<R1, never, X>) => {
+  return <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R | R1, E, A> =>
+    core.uninterruptibleMask((restore) =>
+      pipe(
+        restore(self),
+        core.foldCauseEffect(
+          (cause1) =>
+            pipe(
+              finalizer,
+              core.foldCauseEffect(
+                (cause2) => core.failCause(Cause.sequential(cause1, cause2)),
+                () => core.failCause(cause1)
+              )
+            ),
+          (a) => pipe(finalizer, core.as(a))
+        )
+      )
+    )
+}
+
+/** @internal */
+export const foldEffect = <E, A, R2, E2, A2, R3, E3, A3>(
+  onFailure: (e: E) => Effect.Effect<R2, E2, A2>,
+  onSuccess: (a: A) => Effect.Effect<R3, E3, A3>
+): <R>(self: Effect.Effect<R, E, A>) => Effect.Effect<R | R2 | R3, E2 | E3, A2 | A3> => {
+  return core.foldCauseEffect(
+    (cause) => {
+      const either = Cause.failureOrCause(cause)
+      switch (either._tag) {
+        case "Left": {
+          return onFailure(either.left)
+        }
+        case "Right": {
+          return core.failCause(either.right)
+        }
+      }
+    },
+    onSuccess
+  )
+}
+
+/** @internal */
+export const fromEither = <E, A>(either: Either.Either<E, A>): Effect.Effect<never, E, A> => {
+  switch (either._tag) {
+    case "Left": {
+      return core.fail(either.left)
+    }
+    case "Right": {
+      return core.succeed(either.right)
+    }
+  }
+}
+
+/** @internal */
+export const mapError = <E, E2>(f: (e: E) => E2): <R, A>(self: Effect.Effect<R, E, A>) => Effect.Effect<R, E2, A> => {
+  return core.foldCauseEffect(
+    (cause) => {
+      const either = Cause.failureOrCause(cause)
+      switch (either._tag) {
+        case "Left": {
+          return core.failSync(() => f(either.left))
+        }
+        case "Right": {
+          return core.failCause(either.right)
+        }
+      }
+    },
+    core.succeed
+  )
+}
+
+/** @internal */
+export const sandbox = <R, E, A>(
+  self: Effect.Effect<R, E, A>
+): Effect.Effect<R, Cause.Cause<E>, A> => {
+  return pipe(self, core.foldCauseEffect(core.fail, core.succeed))
+}
