f10db024055507d731e70460ae169d7f1acf277a
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-03 14:00:14 +0000

    keep schedule state internal

--- src/internal/schedule.ts
+++ src/internal/schedule.ts
@@ -1,4 +1,6 @@
+import type * as Effect from "@effect/io/Effect"
 import type * as Schedule from "@effect/io/Schedule"
+import type * as ScheduleDecision from "@effect/io/Schedule/Decision"
 
 /** @internal */
 const ScheduleSymbolKey = "@effect/io/Schedule"
@@ -7,3 +9,24 @@ const ScheduleSymbolKey = "@effect/io/Schedule"
 export const ScheduleTypeId: Schedule.ScheduleTypeId = Symbol.for(
   ScheduleSymbolKey
 ) as Schedule.ScheduleTypeId
+
+/** @internal */
+const variance = {
+  _Env: (_: never) => _,
+  _In: (_: unknown) => _,
+  _Out: (_: never) => _
+}
+
+/** @internal */
+export class ScheduleImpl<S, Env, In, Out> implements Schedule.Schedule<Env, In, Out> {
+  [ScheduleTypeId] = variance
+
+  constructor(
+    readonly initial: S,
+    readonly step: (
+      now: number,
+      input: In,
+      state: S
+    ) => Effect.Effect<Env, never, readonly [S, Out, ScheduleDecision.ScheduleDecision]>
+  ) {}
+}
