6714e475cfca513ddd8781a5fe7fcf13f23ed3f7
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-11-05 13:58:25 +0000

    add Layer primitives

---
+++ src/internal/opCodes/layer.ts
@@ -0,0 +1,47 @@
+/** @internal */
+export const OP_EXTEND_SCOPE = 0 as const
+
+/** @internal */
+export type OP_EXTEND_SCOPE = typeof OP_EXTEND_SCOPE
+
+/** @internal */
+export const OP_FOLD = 1 as const
+
+/** @internal */
+export type OP_FOLD = typeof OP_FOLD
+
+/** @internal */
+export const OP_FRESH = 2 as const
+
+/** @internal */
+export type OP_FRESH = typeof OP_FRESH
+
+/** @internal */
+export const OP_FROM_EFFECT = 3 as const
+
+/** @internal */
+export type OP_FROM_EFFECT = typeof OP_FROM_EFFECT
+
+/** @internal */
+export const OP_SCOPED = 4 as const
+
+/** @internal */
+export type OP_SCOPED = typeof OP_SCOPED
+
+/** @internal */
+export const OP_SUSPEND = 5 as const
+
+/** @internal */
+export type OP_SUSPEND = typeof OP_SUSPEND
+
+/** @internal */
+export const OP_PROVIDE_TO = 6 as const
+
+/** @internal */
+export type OP_PROVIDE_TO = typeof OP_PROVIDE_TO
+
+/** @internal */
+export const OP_ZIP_WITH_PAR = 7 as const
+
+/** @internal */
+export type OP_ZIP_WITH_PAR = typeof OP_ZIP_WITH_PAR
