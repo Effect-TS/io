bbf4b0fea23ad4bb79e121b1cdadb885eae1d1e3
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-11-22 14:31:00 +0000

    add type annotations to DefaultServices

--- src/DefaultServices.ts
+++ src/DefaultServices.ts
@@ -2,8 +2,10 @@
  * @since 1.0.0
  */
 import type * as Clock from "@effect/io/Clock"
+import type * as FiberRef from "@effect/io/FiberRef"
 import * as internal from "@effect/io/internal/defaultServices"
 import type * as Random from "@effect/io/Random"
+import type * as Context from "@fp-ts/data/Context"
 
 /**
  * @since 1.0.0
@@ -15,10 +17,10 @@ export type DefaultServices = Clock.Clock | Random.Random
  * @since 1.0.0
  * @category constructors
  */
-export const liveServices = internal.liveServices
+export const liveServices: Context.Context<DefaultServices> = internal.liveServices
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentServices = internal.currentServices
+export const currentServices: FiberRef.FiberRef<Context.Context<DefaultServices>> = internal.currentServices
