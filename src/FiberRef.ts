f5b22d629db52f58f2270cd6f9c7662170868f63
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-11-22 14:57:40 +0000

    add type annotations to FiberRef

--- src/FiberRef.ts
+++ src/FiberRef.ts
@@ -1,9 +1,22 @@
 /**
  * @since 1.0.0
  */
+import type * as Cause from "@effect/io/Cause"
+import type * as Effect from "@effect/io/Effect"
+import type * as RuntimeFlags from "@effect/io/Fiber/Runtime/Flags"
 import * as core from "@effect/io/internal/core"
 import * as fiberRuntime from "@effect/io/internal/fiberRuntime"
 import * as logger from "@effect/io/internal/logger"
+import type * as Logger from "@effect/io/Logger"
+import type * as LogLevel from "@effect/io/Logger/Level"
+import type * as LogSpan from "@effect/io/Logger/Span"
+import type * as Scope from "@effect/io/Scope"
+import type * as Supervisor from "@effect/io/Supervisor"
+import type * as Context from "@fp-ts/data/Context"
+import type * as Differ from "@fp-ts/data/Differ"
+import type * as HashSet from "@fp-ts/data/HashSet"
+import type * as List from "@fp-ts/data/List"
+import type * as Option from "@fp-ts/data/Option"
 
 /**
  * @since 1.0.0
@@ -51,102 +64,129 @@ export interface Variance<A> {
  * @since 1.0.0
  * @category constructors
  */
-export const make = fiberRuntime.fiberRefMake
+export const make: <A>(
+  initial: A,
+  fork?: (a: A) => A,
+  join?: (left: A, right: A) => A
+) => Effect.Effect<Scope.Scope, never, FiberRef<A>> = fiberRuntime.fiberRefMake
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category constructors
  */
-export const makeWith = fiberRuntime.fiberRefMakeWith
+export const makeWith: <Value>(ref: () => FiberRef<Value>) => Effect.Effect<Scope.Scope, never, FiberRef<Value>> =
+  fiberRuntime.fiberRefMakeWith
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category constructors
  */
-export const makeEnvironment = fiberRuntime.fiberRefMakeEnvironment
+export const makeEnvironment: <A>(
+  initial: Context.Context<A>
+) => Effect.Effect<Scope.Scope, never, FiberRef<Context.Context<A>>> = fiberRuntime.fiberRefMakeEnvironment
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category constructors
  */
-export const makeRuntimeFlags = fiberRuntime.fiberRefMakeRuntimeFlags
+export const makeRuntimeFlags: (
+  initial: RuntimeFlags.RuntimeFlags
+) => Effect.Effect<Scope.Scope, never, FiberRef<RuntimeFlags.RuntimeFlags>> = fiberRuntime.fiberRefMakeRuntimeFlags
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const unsafeMake = core.fiberRefUnsafeMake
+export const unsafeMake: <Value>(
+  initial: Value,
+  fork?: (a: Value) => Value,
+  join?: (left: Value, right: Value) => Value
+) => FiberRef<Value> = core.fiberRefUnsafeMake
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const unsafeMakeHashSet = core.fiberRefUnsafeMakeHashSet
+export const unsafeMakeHashSet: <A>(initial: HashSet.HashSet<A>) => FiberRef<HashSet.HashSet<A>> =
+  core.fiberRefUnsafeMakeHashSet
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const unsafeMakeEnvironment = core.fiberRefUnsafeMakeEnvironment
+export const unsafeMakeEnvironment: <A>(initial: Context.Context<A>) => FiberRef<Context.Context<A>> =
+  core.fiberRefUnsafeMakeEnvironment
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const unsafeMakeSupervisor = fiberRuntime.fiberRefUnsafeMakeSupervisor
+export const unsafeMakeSupervisor: (initial: Supervisor.Supervisor<any>) => FiberRef<Supervisor.Supervisor<any>> =
+  fiberRuntime.fiberRefUnsafeMakeSupervisor
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const unsafeMakePatch = core.fiberRefUnsafeMakePatch
+export const unsafeMakePatch: <Value, Patch>(
+  initial: Value,
+  differ: Differ.Differ<Value, Patch>,
+  fork: Patch,
+  join?: (oldV: Value, newV: Value) => Value
+) => FiberRef<Value> = core.fiberRefUnsafeMakePatch
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category getters
  */
-export const get = core.fiberRefGet
+export const get: <A>(self: FiberRef<A>) => Effect.Effect<never, never, A> = core.fiberRefGet
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const getAndSet = core.fiberRefGetAndSet
+export const getAndSet: <A>(value: A) => (self: FiberRef<A>) => Effect.Effect<never, never, A> = core.fiberRefGetAndSet
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const getAndUpdate = core.fiberRefgetAndUpdate
+export const getAndUpdate: <A>(f: (a: A) => A) => (self: FiberRef<A>) => Effect.Effect<never, never, A> =
+  core.fiberRefgetAndUpdate
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const getAndUpdateSome = core.fiberRefGetAndUpdateSome
+export const getAndUpdateSome: <A>(
+  pf: (a: A) => Option.Option<A>
+) => (self: FiberRef<A>) => Effect.Effect<never, never, A> = core.fiberRefGetAndUpdateSome
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const getWith = core.fiberRefGetWith
+export const getWith: <R, E, A, B>(
+  f: (a: A) => Effect.Effect<R, E, B>
+) => (self: FiberRef<A>) => Effect.Effect<R, E, B> = core.fiberRefGetWith
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const set = core.fiberRefSet
+export const set: <A>(value: A) => (self: FiberRef<A>) => Effect.Effect<never, never, void> = core.fiberRefSet
 
-const _delete = core.fiberRefDelete
+const _delete: <A>(self: FiberRef<A>) => Effect.Effect<never, never, void> = core.fiberRefDelete
+
 export {
   /**
    * @macro traced
@@ -161,142 +201,146 @@ export {
  * @since 1.0.0
  * @category mutations
  */
-export const reset = core.fiberRefReset
+export const reset: <A>(self: FiberRef<A>) => Effect.Effect<never, never, void> = core.fiberRefReset
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const modify = core.fiberRefModify
+export const modify: <A, B>(f: (a: A) => readonly [B, A]) => (self: FiberRef<A>) => Effect.Effect<never, never, B> =
+  core.fiberRefModify
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const modifySome = core.fiberRefModifySome
+export const modifySome: <B, A>(
+  def: B,
+  f: (a: A) => Option.Option<readonly [B, A]>
+) => (self: FiberRef<A>) => Effect.Effect<never, never, B> = core.fiberRefModifySome
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const update = core.fiberRefUpdate
+export const update: <A>(f: (a: A) => A) => (self: FiberRef<A>) => Effect.Effect<never, never, void> =
+  core.fiberRefUpdate
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const updateSome = core.fiberRefUpdateSome
+export const updateSome: <A>(
+  pf: (a: A) => Option.Option<A>
+) => (self: FiberRef<A>) => Effect.Effect<never, never, void> = core.fiberRefUpdateSome
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const updateAndGet = core.fiberRefUpdateAndGet
+export const updateAndGet: <A>(f: (a: A) => A) => (self: FiberRef<A>) => Effect.Effect<never, never, A> =
+  core.fiberRefUpdateAndGet
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const updateSomeAndGet = core.fiberRefUpdateSomeAndGet
+export const updateSomeAndGet: <A>(
+  pf: (a: A) => Option.Option<A>
+) => (self: FiberRef<A>) => Effect.Effect<never, never, A> = core.fiberRefUpdateSomeAndGet
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const locally = core.fiberRefLocally
+export const locally: <A>(
+  value: A
+) => (self: FiberRef<A>) => <R, E, B>(use: Effect.Effect<R, E, B>) => Effect.Effect<R, E, B> = core.fiberRefLocally
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const locallyWith = core.fiberRefLocallyWith
+export const locallyWith: <A>(
+  f: (a: A) => A
+) => (self: FiberRef<A>) => <R, E, B>(use: Effect.Effect<R, E, B>) => Effect.Effect<R, E, B> = core.fiberRefLocallyWith
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const locallyScoped = fiberRuntime.fiberRefLocallyScoped
+export const locallyScoped: <A>(value: A) => (self: FiberRef<A>) => Effect.Effect<Scope.Scope, never, void> =
+  fiberRuntime.fiberRefLocallyScoped
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category mutations
  */
-export const locallyScopedWith = fiberRuntime.fiberRefLocallyScoped
+export const locallyScopedWith: <A>(value: A) => (self: FiberRef<A>) => Effect.Effect<Scope.Scope, never, void> =
+  fiberRuntime.fiberRefLocallyScoped
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentEnvironment = core.currentEnvironment
+export const currentEnvironment: FiberRef<Context.Context<never>> = core.currentEnvironment
 
 /**
  * @macro traced
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentLogAnnotations = core.currentLogAnnotations
+export const currentLogAnnotations: FiberRef<ReadonlyMap<string, string>> = core.currentLogAnnotations
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentLoggers = logger.currentLoggers
+export const currentLoggers: FiberRef<HashSet.HashSet<Logger.Logger<string, any>>> = logger.currentLoggers
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentLogLevel = core.currentLogLevel
+export const currentLogLevel: FiberRef<LogLevel.LogLevel> = core.currentLogLevel
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentLogSpan = core.currentLogSpan
+export const currentLogSpan: FiberRef<List.List<LogSpan.LogSpan>> = core.currentLogSpan
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentRuntimeFlags = fiberRuntime.currentRuntimeFlags
+export const currentRuntimeFlags: FiberRef<RuntimeFlags.RuntimeFlags> = fiberRuntime.currentRuntimeFlags
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentScheduler = core.currentScheduler
+export const currentParallelism: FiberRef<Option.Option<number>> = core.currentParallelism
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentParallelism = core.currentParallelism
+export const currentSupervisor: FiberRef<Supervisor.Supervisor<any>> = fiberRuntime.currentSupervisor
 
 /**
  * @since 1.0.0
  * @category fiberRefs
  */
-export const currentSupervisor = fiberRuntime.currentSupervisor
-
-/**
- * @since 1.0.0
- * @category fiberRefs
- */
-export const forkScopeOverride = core.forkScopeOverride
-
-/**
- * @since 1.0.0
- * @category fiberRefs
- */
-export const interruptedCause = core.interruptedCause
+export const interruptedCause: FiberRef<Cause.Cause<never>> = core.interruptedCause
