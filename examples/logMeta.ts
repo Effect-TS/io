d115f4d49116386e135e3645b385545520c5acc4
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-01 19:42:36 +0000

    add logMeta example

---
+++ examples/logMeta.ts
@@ -0,0 +1,30 @@
+import * as Effect from "@effect/io/Effect"
+import * as FiberRef from "@effect/io/FiberRef"
+import * as FiberRefs from "@effect/io/FiberRefs"
+import * as Logger from "@effect/io/Logger"
+import { pipe } from "@fp-ts/data/Function"
+
+type LogMeta = Record<string, string>
+
+const logMeta = FiberRef.unsafeMake<LogMeta>({})
+
+const logInfoWithMeta = (message: string, data: LogMeta) => FiberRef.locally(data)(logMeta)(Effect.logInfo(message))
+
+const customLogger = Logger.make<string, void>(
+  (fiberId, logLevel, message, cause, context, spans, annotations, runtime) => {
+    const meta = FiberRefs.getOrDefault(logMeta)(context)
+    const formatted = Logger.stringLogger.log(fiberId, logLevel, message, cause, context, spans, annotations, runtime)
+    console.log(formatted, { meta })
+  }
+)
+
+const program = Effect.gen(function*($) {
+  yield* $(logInfoWithMeta("message1", { foo: "bar" }))
+})
+
+const main = pipe(
+  program,
+  Effect.provideSomeLayer(Logger.replace(Logger.defaultLogger, customLogger))
+)
+
+Effect.unsafeFork(main)
