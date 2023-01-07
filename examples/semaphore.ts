762bf09a9e63ae9cfa06a7afa88aea4b1ff7b4f3
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-07 06:38:15 +0000

    improve liteweight semaphore

--- examples/semaphore.ts
+++ examples/semaphore.ts
@@ -1,18 +1,22 @@
+import * as Ca from "@effect/io/Cause"
 import * as E from "@effect/io/Effect"
+import * as Ex from "@effect/io/Exit"
 import * as D from "@fp-ts/data/Duration"
 
 const program = E.gen(function*($) {
-  const withPermits = yield* $(E.makeSemaphore(4))
+  const sem = yield* $(E.makeSemaphore(4))
 
   yield* $(
-    E.collectAllPar([0, 1, 2, 3].map((n) => withPermits(2)(E.delay(D.seconds(2))(E.log(`process: ${n}`)))))
+    E.collectAllPar([0, 1, 2, 3].map((n) => sem.withPermits(2)(E.delay(D.seconds(2))(E.log(`process: ${n}`)))))
   )
 
   yield* $(
-    E.collectAllPar([0, 1, 2, 3].map((n) => withPermits(2)(E.delay(D.seconds(2))(E.log(`process: ${n}`)))))
+    E.collectAllPar([0, 1, 2, 3].map((n) => sem.withPermits(2)(E.delay(D.seconds(2))(E.log(`process: ${n}`)))))
   )
 })
 
 E.unsafeRun(program, (ex) => {
-  console.log(ex)
+  if (Ex.isFailure(ex)) {
+    console.log(Ca.pretty()(ex.cause))
+  }
 })
