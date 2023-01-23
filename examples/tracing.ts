a77f673d1c45b997ea6dce07146a37ac8cae9cb2
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-23 18:29:43 +0000

    Improve example

--- examples/tracing.ts
+++ examples/tracing.ts
@@ -1,9 +1,15 @@
 import * as Effect from "@effect/io/Effect"
 
+const message = (n: number) => {
+  if (n < 10) {
+    throw new Error("well...")
+  }
+}
+
 const program = Effect.gen(function*($) {
   const a = yield* $(Effect.succeed(0))
   const b = yield* $(Effect.succeed(a + 1))
-  return yield* $(Effect.fail(new Error(`r: ${b}`)))
+  return message(b)
 })
 
 Effect.unsafeFork(Effect.catchAllCause(program, Effect.logErrorCause))
