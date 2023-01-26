9dd6200fb8a8fa75e806aa9ee24fdc4b11a054c9
Michael Arnaldi <arnaldimichael@gmail.com>
2023-01-26 00:46:56 +0000

    Improve example

--- examples/tracing.ts
+++ examples/tracing.ts
@@ -1,10 +1,15 @@
 import * as Effect from "@effect/io/Effect"
 import { millis } from "@fp-ts/data/Duration"
 
+class MyError {
+  readonly _tag = "A"
+  readonly message = "Ok!"
+}
+
 const program = Effect.tuplePar(
   Effect.delay(Effect.succeed(0), millis(500)),
   Effect.delay(Effect.succeed(0), millis(500)),
-  Effect.delay(Effect.failSync(() => new Error("OK")), millis(500)),
+  Effect.delay(Effect.failSync(() => new MyError()), millis(500)),
   Effect.delay(Effect.succeed(0), millis(500))
 )
 
