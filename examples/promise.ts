ab5eba91d3398961d95b1a7c5b65117c079402b9
Michael Arnaldi <arnaldimichael@gmail.com>
2023-02-01 00:06:23 +0000

    Fix tracing of promise based constructors

---
+++ examples/promise.ts
@@ -0,0 +1,9 @@
+import * as Effect from "@effect/io/Effect"
+
+const program = Effect.promise(() =>
+  new Promise((_, rej) => {
+    rej(new Error("ok"))
+  })
+)
+
+Effect.runFork(Effect.catchAllCause(program, Effect.logErrorCause))
